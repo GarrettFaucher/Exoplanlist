efc5a1f6a0c16c59b7aa8481bf8632e334951c11
Garrett Faucher <garrettfaucher@gmail.com>
2020-11-14 21:05:18 -0500

    Linked buttons

--- Exoplanlist/ItemsViewController.swift
+++ Exoplanlist/ItemsViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 class ItemsViewController: UITableViewController {
     var itemStore: ItemStore!
     
+    @IBAction func addNewItem(_ sender: UIButton) {
+        
+    }
+    @IBAction func toggleEditingMode(_ sender: UIButton) {
+        
+    }
+    
     override func tableView(_ tableView: UITableView,
                             numberOfRowsInSection section: Int) -> Int {
         return itemStore.allItems.count
@@ -17,8 +24,9 @@ class ItemsViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView,
                             cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        // Create an instance of UITableViewCell with default appearance
-        let cell = UITableViewCell(style: .value1, reuseIdentifier: "UITableViewCell")
+        // Get a new or recycled cell
+        let cell = tableView.dequeueReusableCell(withIdentifier: "UITableViewCell",
+                                                 for: indexPath)
         
         // Set the text on the cell with the description of the item
         // that is at the nth index of items, where n = row this cell
