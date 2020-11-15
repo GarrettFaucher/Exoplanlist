9c6decae4f8ec50a62803b6724ccd09b76982144
Garrett Faucher <garrettfaucher@gmail.com>
2020-11-14 22:20:22 -0500

    Better labels

---
+++ Exoplanlist/ItemCell.swift
@@ -0,0 +1,16 @@
+//
+//  ItemCell.swift
+//  Exoplanlist
+//
+//  Created by Garrett on 11/14/20.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+    
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var distanceLabel: UILabel!
+    @IBOutlet var discoveryLabel: UILabel!
+    
+}
